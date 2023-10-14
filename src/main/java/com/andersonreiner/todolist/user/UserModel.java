758cbaa4f2b03c235d42ed43d62dc381789fa082
AndersonReiner <108676604+AndersonReiner@users.noreply.github.com>
2023-10-14 19:12:14 -0400

    Firts Commit

---
+++ src/main/java/com/andersonreiner/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package com.andersonreiner.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+}
