b2b73a852a214d5708f795d22de74e51f2fcc915
AndersonReiner <108676604+AndersonReiner@users.noreply.github.com>
2023-10-14 23:30:59 -0400

    Tenth Commit user and password validation

--- src/main/java/com/andersonreiner/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/andersonreiner/todolist/filter/FilterTaskAuth.java
@@ -2,8 +2,13 @@ package com.andersonreiner.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.andersonreiner.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,32 +17,53 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
 
+    @Autowired
+    private IUserRepository iuserrepository;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
                 
-                // pegar a autenticação(usuário e senha)
-                var authorization = request.getHeader("Authorization");
+                var servletPath = request.getServletPath();
+
+                if(servletPath.equals("/tasks/")){
+                    // pegar a autenticação(usuário e senha)
+                     var authorization = request.getHeader("Authorization");
                 
 
-                var user_password_encrypted = authorization.substring("Basic".length()).trim();
+                    var user_password_encrypted = authorization.substring("Basic".length()).trim();
 
-                byte[] user_password = Base64.getDecoder().decode(user_password_encrypted);
+                    byte[] user_password = Base64.getDecoder().decode(user_password_encrypted);
 
-                var user_and_password = new String(user_password);
+                    var user_and_password = new String(user_password);
 
-                String[] fim_user_and_password = user_and_password.split(":");
-                String username = fim_user_and_password[0];
-                String password = fim_user_and_password[1];
+                    String[] fim_user_and_password = user_and_password.split(":");
+                    String username = fim_user_and_password[0];
+                    String password = fim_user_and_password[1];
+                    System.out.println(username);
+                    System.out.println(password);
 
-                System.out.println(username);
-                System.out.println(password);
+                    // validar usário e senha
+                    var user = this.iuserrepository.findByUsername(username);
+                    if(user == null){
+                        response.sendError(401, "Usuário não tem permissão");
+                    }else{
+                        var password_verify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        if(password_verify.verified){
+                            filterChain.doFilter(request, response);
+                        }else{
+                            response.sendError(401);
+                        }
+                    }
 
-                // validar usário
+                }else{
+                    filterChain.doFilter(request, response);
+                }
 
-                // validar senha
 
-                filterChain.doFilter(request, response);
-    }
+                
+
+                
+            }
 
 }
