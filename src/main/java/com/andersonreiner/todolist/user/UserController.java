514dc4c64ac9c85efaa60401fed3e0a4bb76fef3
AndersonReiner <108676604+AndersonReiner@users.noreply.github.com>
2023-10-14 20:52:47 -0400

    Second Commit created user table

--- src/main/java/com/andersonreiner/todolist/user/UserController.java
+++ src/main/java/com/andersonreiner/todolist/user/UserController.java
@@ -1,4 +1,5 @@
 package com.andersonreiner.todolist.user;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
     
+    //Essa anotação fica responsável por instanciar o objeto, cuidar do ciclo de vida.
+    @Autowired
+    private IUserRepository iuserRepository;
+
     @PostMapping("/")
-    public void create (@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public UserModel create (@RequestBody UserModel userModel){
+        var userCreated = this.iuserRepository.save(userModel);
+        return userCreated;
+    
     }
 }
