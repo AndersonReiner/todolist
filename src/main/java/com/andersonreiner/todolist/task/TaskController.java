3e06f0ebd72a56dfd916b1517c16e79a2a215389
AndersonReiner <108676604+AndersonReiner@users.noreply.github.com>
2023-10-15 18:49:57 -0400

    final

--- src/main/java/com/andersonreiner/todolist/task/TaskController.java
+++ src/main/java/com/andersonreiner/todolist/task/TaskController.java
@@ -1,10 +1,7 @@
 package com.andersonreiner.todolist.task;
-
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.andersonreiner.todolist.utils.Utils;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -73,7 +68,5 @@ public class TaskController{
         Utils.copyNonNullProperties(taskModel, task);
         var taskUpdate = this.itaskrepository.save(task);
         return ResponseEntity.ok().body(taskUpdate);
-    }
-
-    
+    } 
 }
